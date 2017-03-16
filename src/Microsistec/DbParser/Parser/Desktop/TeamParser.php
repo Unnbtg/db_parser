ad697741a8edc75c303a1b5121f7e4ce13c74a13
Mueller <ervinomueller@outlook.com>
2017-03-16 17:48:59 -0300

    [DEV]

---
+++ src/Microsistec/DbParser/Parser/Desktop/TeamParser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Microsistec\DbParser\Parser\Desktop;
+
+use Microsistec\DbParser\Parser\ParserAbstract;
+use Microsistec\DbParser\Parser\ParserInterface;
+use Microsistec\DbParser\Team;
+
+class TeamParser extends ParserAbstract implements ParserInterface
+{
+    public function parse($model, $domain = "", $account = "")
+    {
+        $team         = new Team();
+        $team->maintence_id = $model->id;
+        $team->id           = $model->id;
+        $team->name         = $model->name;
+        $team->status       = 0;
+
+        return $team;
+    }
+
+}
---
+++ src/Microsistec/DbParser/Team.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mueller
+ * Date: 16/03/17
+ * Time: 10:49
+ */
+
+namespace Microsistec\DbParser;
+/**
+ * Class Team
+ * @package Microsistec\DbParser
+ *
+ *
+ * @property integer         $id
+ * @property string          $name
+ * @property integer         $status
+ *
+ **/
+
+class Team extends AbstractParsedType
+{
+
+}
