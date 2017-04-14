f6132e27043eab463bf9430cf9e5e2771093e642
Mueller <muller@microsistec.com.br>
2017-04-14 12:25:06 -0300

    [DEV]

--- src/Microsistec/DbParser/Parser/Desktop/FormParser.php
+++ src/Microsistec/DbParser/Parser/Desktop/FormParser.php
@@ -32,11 +32,14 @@ class FormParser extends ParserAbstract implements ParserInterface
         $form->profile->dorm_count        = (int)$model->property_dorm;
         $form->profile->for_sale          = $model->property_finality == 0 ? true : false;
         $form->profile->for_rent          = $model->property_finality == 1 ? true : false;
-        $form->profile->max_value         = str_replace(',', '.', $model->property_max_value);
+        $form->profile->max_value         = null;
+        if(!empty($model->property_max_value)) {
+            $form->profile->max_value         = str_replace(',', '.', $model->property_max_value);
+        }
         $form->profile->neighborhoods     = [$model->property_neighborhoods];
         $form->profile->parking_lot_count = (int)$model->property_parking_lots;
         $form->profile->suit_count        = (int)$model->property_suite;
-        $form->profile->types             = [$model->property_type];
+        $form->profile->types             = !is_null($model->property_type) ? [$model->property_type] : [];
 
         $form->notes = [];
         if(!empty($model->note)){
